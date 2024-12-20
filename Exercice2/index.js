15374fec5417b8417ac408605beb0449df3933f2
Rachel Marquant <marquantrachel@gmail.com>
2024-12-20 14:07:18 +0100

    Finition de l'exercice 2

--- Exercice2/index.js
+++ Exercice2/index.js
@@ -1,49 +1,19 @@
 console.log("yes"); // je teste si j'ai reussi a lier js et html
 
-
-// je cree des fonctions pour pouvoir avoir leur fond
-function cyan (){
-    var carreC = document.getElementById("cyan"); // je recupere la class carre
-    const cssObj = window.getComputedStyle(carreC, null); // je recupere le css du carre
+// je cree des fonctions pour pouvoir avoir leur fond 
+function clique (id){
+    var carre = document.getElementById(id); // je recupere la class carre
+    const cssObj = window.getComputedStyle(carre, null); // je recupere le css du carre
     var fond = cssObj.getPropertyValue("background-color"); // je recupere la couleur du fond
-    console.log(fond);
-    addElement(fond);
-}
+    var newDiv = document.getElementById("copie");  // je recupere la div copie
 
-
-function maroon (){
-    var carreM = document.getElementById("maroon"); // je recupere la class carre
-    const cssObj = window.getComputedStyle(carreM, null); // je recupere le css du carre
-    var fond = cssObj.getPropertyValue("background-color"); // je recupere la couleur du fond
-    console.log(fond);
-    addElement(fond);
-}
-
-
-function vert (){
-    var carreV = document.getElementById("vert"); // je recupere la class carre
-    const cssObj = window.getComputedStyle(carreV, null); // je recupere le css du carre
-    var fond = cssObj.getPropertyValue("background-color"); // je recupere la couleur du fond
-    console.log(fond);
-    addElement(fond);
-}
-
-
-function jaune (){
-    var carreJ = document.getElementById("jaune"); // je recupere la class carre
-    const cssObj = window.getComputedStyle(carreJ, null); // je recupere le css du carre
-    var fond = cssObj.getPropertyValue("background-color"); // je recupere la couleur du fond
-    console.log(fond);
-    addElement(fond);
-}
-
-
-function addElement(fond){
-    const newDiv = document.createElement("div"); // je cree une nouvelle div
-    newDiv.style.width="200px"; // je rajoute une longueur de 200 pixels
-    newDiv.style.height="200px"; // je reojoute une hauteur de 200 pixels
-    newDiv.style.backgroundColor=fond; // je change la couleur de fond   
-    const newContent = document.createTextNode(fond); // je rajoute le fond en texte
-    const currentDiv = document.getElementById("carres");
-    document.body.insertBefore(newDiv, currentDiv);
+    newDiv.style.backgroundColor=fond; // je change la couleur de fond
+    newDiv.innerHTML = fond; // je rajoute a la di copie du texte
+    if (fond == "rgb(128, 0, 0)" || fond=="rgb(0, 128, 0)") // si le fond est rouge ou vert
+    {
+        newDiv.style.color = 'white'; // je change la couleur de police
+    }
+    else {
+        newDiv.style.color = 'black'; // je change la couleur de police
+    }
 }
