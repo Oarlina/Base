2a5db5fa1cbee23b888deccfefe88a76aa686a35
Rachel Marquant <marquantrachel@gmail.com>
2024-12-23 12:03:14 +0100

    Amelioration exo 5

--- Exercice5/index.js
+++ Exercice5/index.js
@@ -1,19 +1,48 @@
 console.log("yes"); // je teste si j'ai reussi a lier js et html
 
-// alert(quotes);
 
-console.log(quotes);
-
-
-for (let quote in quotes) { // je parcours les citations
+for (let quote in quotes) 
+{ // je parcours les citations
 
     var div = document.createElement("div"); // je cree une nouvelle div
-    div = document.createTextNode(quotes[quote]); // je lui rajoute du texte
-    div += document.createTextNode(quote); // et un deuxieme texte
-    console.log(div);
-    console.log(quote + ':' + quotes[quote]);
-   }
+    var q = document.createElement("q"); // je cree un nouveau paragraphe
+    var p = document.createElement("p"); // je cree un deuxieme paragraphe
+    var favori = document.createElement("div"); // je cree un deuxieme div pour le favori du favori
+    
+    div.className = "citation"; // je donne a la div la class citation (gerer en css)
+    q.className = "text"; // je donne a la citation la class text (gerer en css)
+    p.className = "auteur"; // je donne au paragraphe la class auteur (gerer en css)
+    favori.className = "favori"; // je donne a la div la class favori (gerer en css)
+    favori.id = "non"; // je donne a la div l'id non 
+    
+    titre= document.createTextNode(quotes[quote]); // je rajoute du textre au premier paragraphe
+    q.appendChild(titre); // je met la citation dans un paragraphe
+    div.appendChild(q); // je rajoute a div l'enfant q
+    
+    auteur= document.createTextNode(quote); // je rajoute du textre au premier paragraphe
+    p.appendChild(auteur); // je met la citation dans un paragraphe
+    div.appendChild(p); // je rajoute a div l'enfant p
+    
+    var icon = "<i class='fa-regular fa-heart'></i>";
+    favori.innerHTML = icon; // je rajoute du texte a favori
+    div.appendChild(favori); // je rajoute a div l'enfant favori
+    
+    document.body.appendChild(div); // je rajoute la div au body
+}
+var citations = document.querySelectorAll(".citation");  // je recupere un tableau de toutes les citations
+citations.forEach(citation => {
+    var ic = citation.children(".favori");
+    console.log(citation.id);
+    favori.addEventListener("click", function(){
+        if (favori.id == "non")
+            {
+                favori.innerHTML = "<i class='fa-solid fa-heart'></i>";
+                favori.id = "oui";
+            }else 
+            {
+                favori.innerHTML = "<i class='fa-regular fa-heart'></i>";
+                favori.id="non";
+            }
+    }); // j'ajoute une fonction click pour mettre un like
+});
 
-// tab.forEach(quote => {
-//     console.log(quote);
-// });
--- Exercice5/quotes.js
+++ Exercice5/quotes.js
@@ -1,8 +1,14 @@
 var quotes ={
-    "Serge Karamov" : "Barrez vous ***** de mines.",
+    "Serge Karamov" : "Barrez vous ***** de mimes.",
     "Georges Abitol" : "Le train de tes injures roule sur le rail de mon indifférence.",
     "Huvert Bonisseur" : "Ca ne prenait pas beaucoup plus de temps.",
-    "Moulinier" : "L'habit ne fait pas le moine, mais il fait l'argent... Même non titularisé.",
+    "Moulinier" : "L'habit ne fait pas le moine, mais il fait l'agent... Même non titularisé.",
     "Orson Welles" : "C'est du vol et du plagiat. J'aime pas trop les voleurs et...",
+    "Jeff Goodell " : "If you are interested enough in the climate crisis to read this post, you probably know that 2 degrees Centigrade of warming (or 3.6 degrees Fahrenheit) is the widely acknowledged threshold for 'dangerous' 'climate change.'",
+    "Rashida Jones" : "I know my mom said as early as she can remember letting me watch TV, my one treat a week when I was like 6 was to stay up and watch 'Saturday Night Live.'",
+    "Anwar Sadat" : "There can be hope only for a society which acts as one big family, not as many separate ones.",
+    "Isaac Asimov": "One, a robot may not injure a human being, or through inaction, allow a human being to come to harm; Two, a robot must obey the orders given it by human beings except where such orders would conflict with the First Law; Three, a robot must protect its own existence as long as such protection does not conflict with the First or Second Laws.",
+    "Vanilla Ice" : "I use the music to vent, and a lot of the stuff that I am writing about or was writing about contained a lot of anger and anxiety, stress and depression, so that's how the album came out so dark.",
+    "James Baldwin" : "It is certain, in any case, that ignorance, allied with power, is the most ferocious enemy justice can have."
 };
 
